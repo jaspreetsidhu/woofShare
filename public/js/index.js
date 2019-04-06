fb16e99eae458810b9b552a72bb3b11e10addf71
Zach Selindh <zachselindh@gmail.com>
2019-04-06 11:07:51 -0500

    Delete this

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,17 @@
-$(".header").on("click", function(){
-  window.location.href = '/'; 
+$(".header").on("click", function() {
+  window.location.href = "/";
 });
+
+$(".fa-heart").on("click", function() {
+  console.log($(this).data("dogid"));
+  if ($(this).hasClass("fas")) {
+    $(this).removeClass("fas");
+    $(this).addClass("far");
+  } else if ($(this).hasClass("far")) {
+    $(this).removeClass("far");
+    $(this).addClass("fas");
+  }
+});
+
+// get favorites from local storage or empty array
+var favorites = JSON.parse(localStorage.getItem("favorites")) || [];
