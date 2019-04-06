a1623ac1d85cde690c1d96bb844bb57a5d132a3f
chandni <chandnibpatel@gmail.com>
2019-04-05 23:37:48 -0500

    added session to save authtoken

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 const passport = require('passport');  
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const session = require('express-session');
 
 
 var app = express();
@@ -12,6 +13,34 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 app.use(express.static('public')); 
+// Add session support
+app.use(session({  
+  secret: process.env.SESSION_SECRET || 'default_session_secret',
+  resave: false,
+  saveUninitialized: false,
+}));
+app.use(passport.initialize());  
+app.use(passport.session());  
+
+
+passport.serializeUser((user, done) => {  
+  done(null, user);
+});
+
+passport.deserializeUser((userDataFromCookie, done) => {  
+  done(null, userDataFromCookie);
+});
+
+// Checks if a user is logged in
+const accessProtectionMiddleware = (req, res, next) => {  
+  if (req.isAuthenticated()) {
+    next();
+  } else {
+    res.status(403).json({
+      message: 'must be logged in to continue',
+    });
+  }
+};
 
 // Set up passport strategy
 passport.use(new GoogleStrategy(  
@@ -29,7 +58,7 @@ passport.use(new GoogleStrategy(
 ));
 
 
-var HTMLroutes = require('./routes/htmlRoutes');
+
 app.get('/auth/google', passport.authenticate('google'));  
 // This is where Google sends users once they authenticate with Google
 
@@ -37,9 +66,19 @@ app.get('/auth/google/callback',
   passport.authenticate('google', { failureRedirect: '/', session: false }),
   (req, res) => {
     console.log('wooo we authenticated, here is our user object:', req.user);
-    res.json(req.user);
+  //  res.json(req.user);
+   res.redirect('/');
   }
 );
+app.get('/protected', accessProtectionMiddleware, (req, res) => {  
+  res.json({
+    message: 'You have accessed the protected endpoint!',
+    yourUserInfo: req.user,
+  });
+});
+
+var HTMLroutes = require('./routes/htmlRoutes');
+
 app.use(HTMLroutes);
 
 
