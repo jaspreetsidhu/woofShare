212007fc8af6d2f22d51ca1db99e49ecfd644524
Zach Selindh <zachselindh@gmail.com>
2019-04-06 13:15:06 -0500

    Added partials and DB connection.

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,17 +1,21 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var connection = require("../config/connection");
 
-
-router.get("/", function (req, res) {
-    res.render("index");
+router.get("/", function(req, res) {
+  res.render("index");
 });
 
-router.get("/signUp", function (req, res) {
-    res.render("signUp");
+router.get("/signUp", function(req, res) {
+  res.render("signUp");
 });
 
-router.get("/gallery", function (req, res) {
-    res.render("gallery");
+router.get("/gallery", function(req, res) {
+  connection.query("SELECT * FROM doggie", function(err, data) {
+    if (err) throw err;
+    console.log(data);
+    res.render("gallery", { dogs: data });
+  });
 });
 
 module.exports = router;
