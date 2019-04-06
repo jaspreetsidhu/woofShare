5509cf19fea18046ecd7d436fa2373bc4eafd42b
chandni <chandnibpatel@gmail.com>
2019-04-05 22:33:20 -0500

    Google Auth changes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-
+//router.get('/auth/google', passport.authenticate('google'));  
 router.get("/", function (req, res) {
     res.render("index");
 });
--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 var express = require("express");
+const passport = require('passport');  
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -9,6 +12,22 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 app.use(express.static('public')); 
+
+// Set up passport strategy
+passport.use(new GoogleStrategy(  
+  {
+    clientID: process.env.GOOGLE_OAUTH_TEST_APP_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_OAUTH_TEST_APP_CLIENT_SECRET,
+    callbackURL: 'https://polar-basin-98786.herokuapp.com/auth/google/callback',
+    scope: ['email'],
+  },
+  // This is a "verify" function required by all Passport strategies
+  (accessToken, refreshToken, profile, cb) => {
+    console.log('Our user authenticated with Google, and Google sent us back this profile info identifying the authenticated user:', profile);
+    return cb(null, profile);
+  },
+));
+
 
 var HTMLroutes = require('./routes/htmlRoutes');
 
