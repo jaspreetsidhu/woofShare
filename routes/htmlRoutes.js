16d86444ed4be0bef9f5f7d11dc728178cc00c76
Trent Davis <trentdavisinc@gmail.com>
2019-04-09 19:53:20 -0500

    .

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -29,7 +29,7 @@ router.get("/confirmation/:dogId", function(req, res) {
 });
 
 router.get("/user-profile", function (req, res) {
-  res.render("userProfile");
+  res.render("userProfile"); 
 });
 
 
