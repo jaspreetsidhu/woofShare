51ce36e97c2096dc63eac48a9fb78d163f24e31c
Jaspreet <jspreetsidhu@gmail.com>
2019-04-12 15:36:21 -0500

    Formatted Document

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,4 @@
-
-$(function(){
+$(function () {
 
   var reviewBox = $('#post-review-box');
   var newReview = $('#new-review');
@@ -7,25 +6,21 @@ $(function(){
   var closeReviewBtn = $('#close-review-box');
   var ratingsField = $('#ratings-hidden');
 
-  openReviewBtn.click(function(e)
-  {
-    reviewBox.slideDown(400, function()
-      {
-        $('#new-review').trigger('autosize.resize');
-        newReview.focus();
-      });
+  openReviewBtn.click(function (e) {
+    reviewBox.slideDown(400, function () {
+      $('#new-review').trigger('autosize.resize');
+      newReview.focus();
+    });
     openReviewBtn.fadeOut(100);
     closeReviewBtn.show();
   });
 
-  closeReviewBtn.click(function(e)
-  {
+  closeReviewBtn.click(function (e) {
     e.preventDefault();
-    reviewBox.slideUp(300, function()
-      {
-        newReview.focus();
-        openReviewBtn.fadeIn(200);
-      });
+    reviewBox.slideUp(300, function () {
+      newReview.focus();
+      openReviewBtn.fadeIn(200);
+    });
     closeReviewBtn.hide();
   });
 });
